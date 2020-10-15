aae87a8bbc8f535f595d2d1ca08bac79bd034197
Karthikeyan Vaithilingam <seenukarthi@gmail.com>
2020-10-15 08:58:02 +0400

    Minor Formatting.
    Signed-off-by: Karthikeyan Vaithilingam <seenukarthi@gmail.com>

--- src/main/java/com/seenukarthi/tldr/Parser.java
+++ src/main/java/com/seenukarthi/tldr/Parser.java
@@ -10,8 +10,8 @@ import static com.diogonunes.jcolor.Ansi.*;
 @Slf4j
 public class Parser {
 
-    private static final Attribute[] heading = {WHITE_TEXT(),BOLD()};
-    private static final Attribute[] blockQuote = {YELLOW_TEXT(),ITALIC()};
+    private static final Attribute[] heading = {WHITE_TEXT(), BOLD()};
+    private static final Attribute[] blockQuote = {YELLOW_TEXT(), ITALIC()};
     private static final Attribute[] code = {RED_TEXT()};
 
     public static void parse(String fullDoc) {
@@ -25,12 +25,12 @@ public class Parser {
                 log.info(colorize(string.substring(1).trim(), heading));
                 log.info("");
             } else if (string.startsWith("`")) {
-                log.info(parseExample("    "+string.substring(1, string.lastIndexOf('`'))));
+                log.info(parseExample("    " + string.substring(1, string.lastIndexOf('`'))));
                 log.info("");
             } else if (string.startsWith(">")) {
                 log.info(colorize(string.substring(1).trim(), blockQuote));
-            }else if(!StringUtils.isEmpty(string.trim())){
-                if(toLeaveABlankLine){
+            } else if (!StringUtils.isEmpty(string.trim())) {
+                if (toLeaveABlankLine) {
                     log.info("");
                     toLeaveABlankLine = false;
                 }
@@ -44,9 +44,9 @@ public class Parser {
         String[] strings = str.split("\\s");
         StringBuilder builder = new StringBuilder();
         for (String string : strings) {
-            String s = colorize(string,code);
-            s = s.replaceAll("\\{\\{","\u001B[34m");
-            s = s.replaceAll("}}","\u001B[31m");
+            String s = colorize(string, code);
+            s = s.replaceAll("\\{\\{", "\u001B[34m");
+            s = s.replaceAll("}}", "\u001B[31m");
             builder.append(s).append(" ");
         }
         return builder.toString();
